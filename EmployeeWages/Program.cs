2dc86bfd67f750cadc9ecbe71d4e3628ef1599cf
Arshad <arshadpinjari666@gmail.com>
2021-05-05 20:28:45 +0530

    EmpAttendence

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -6,7 +6,17 @@ namespace EmployeeWages
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation");
+            int IS_Full_TIME = 1;
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            if (empcheck == IS_Full_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
