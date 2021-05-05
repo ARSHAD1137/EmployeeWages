8a98fcf111e3d54e60ed4f193696465822ede731
Arshad <arshadpinjari666@gmail.com>
2021-05-05 20:35:32 +0530

    Added wages

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -6,17 +6,26 @@ namespace EmployeeWages
     {
         static void Main(string[] args)
         {
-            int IS_Full_TIME = 1;
+            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+            //varibles
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
-            int empcheck = random.Next(0, 2);
-            if (empcheck == IS_Full_TIME)
+            // computation
+            int empcheck = random.Next(0, 3);
+            if (empcheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
-            else
+            else if
             {
-                Console.WriteLine("Employee is Absent");
+                empHrs = 4;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("EmpWage:" + empWage);
+
         }
     }
 }
