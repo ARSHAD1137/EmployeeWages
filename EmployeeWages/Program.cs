75afed9b3fb8fc7293f00fa6e337ce4add9e23f0
Arshad <arshadpinjari666@gmail.com>
2021-05-06 16:55:27 +0530

    Added.SwichCase

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -5,25 +5,36 @@ namespace EmployeeWages
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
             int IS_FULL_TIME = 1;
+            int PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
-          
+
+            // Variable
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            
-            int empcheck = random.Next(0, 2);
-            if (empcheck == IS_FULL_TIME)
+
+            //Computation
+            int empcheck = random.Next(0, 3);
+            switch (empcheck)
             {
-                empHrs = 8;
+                case 1:
+                    empHrs = 8;
+                    empWage = empHrs * EMP_RATE_PER_HOUR;
+                    break;
+                case 2:
+                    empHrs = 4;
+                    empWage = empHrs * EMP_RATE_PER_HOUR;
+                    break;
+                case 3:
+                    empHrs = 0;
+                    empWage = empHrs * EMP_RATE_PER_HOUR;
+                    break;
             }
-            else 
-            {
-                empHrs = 0;
-            }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
+
             Console.WriteLine("EmpWage:" + empWage);
+
 
         }
     }
